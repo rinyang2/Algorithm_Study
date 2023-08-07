1d680116cb36f7fd33919032b5e02c3124e5c21a
rinyang2 <jinsk1884@gmail.com>
2023-08-07 21:00:31 +0900

    .

--- BOJ/2000~9999/2661.cpp
+++ BOJ/2000~9999/2661.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-const int MAX = 88;
+const int MAX = 81;
 int N;
 char s[MAX];
 
 bool valid(char s[], int level) {
 	int length = level;
 	//배열을 1부터 길이의 절반사이즈까지 i단위씩 잘라볼 겁니다
-	for (int i = 1; i <= length /2; i++) {
+	for (int i = 1; i <= length / 2; i++) {
 		//배열을 i단위로 자를건데 각 단위배열 첫 index가 j입니다
-		for (int j = 0; j < length - i*2+1; j += 1) {
+		for (int j = 0; j < length - i * 2 + 1; j += 1) {
 			bool t = true;
 			for (int k = 0; k < i; k++) {
 				//단위 배열 중 하나라도 불일치하면 break
@@ -31,8 +31,11 @@ bool valid(char s[], int level) {
 void good(int level) {
 
 	if (level == N) {
-		cout << s << endl;
-		exit(0);
+		if (valid(s, level)) {
+			cout << s << endl;
+			exit(0);
+		}
+		return;
 	}
 
 	for (int i = 1; i <= 3; i++) {
@@ -40,7 +43,7 @@ void good(int level) {
 			continue;
 		}
 		s[level] = '0' + i;
-		if(valid(s, level))
+		if (valid(s, level))
 			good(level + 1);
 	}
 }
